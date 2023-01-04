cf170266b4e08fbac51ad79e36a5252f73c5484d
Jiayi Wang <z5290566@unsw.edu.au>
2023-01-04 16:59:19 +1100

    push

--- components/WorkoutCard.js
+++ components/WorkoutCard.js
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SparklesIcon } from "react-native-heroicons/solid";
 
 const WorkoutCard = ({id, title, length}) => {
   return (
     <View className='flex-row'>
       <Text className='text-xl font-bold'>{title}</Text>
-      
+      <SparklesIcon />
       <Text className='text-pink-500'>{length}</Text>
     </View>
   )
