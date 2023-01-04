81b6415201edee9d7f00d55d2bb917b0058c95c8
Jiayi Wang <z5290566@unsw.edu.au>
2023-01-04 18:25:15 +1100

    add new menu

--- components/WorkoutCard.js
+++ components/WorkoutCard.js
@@ -1,14 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 import {ClockIcon } from "react-native-heroicons/solid";
 
 const WorkoutCard = ({id, title, length}) => {
   return (
-    <View className='flex-row items-center space-x-2 px-5 py-2'>
+    <TouchableOpacity className='flex-row items-center space-x-5 px-5 my-5 mx-5 bg-purple-200 rounded-xl h-20'>
       <Text className='text-2xl font-bold flex-1'>{title}</Text>
-      <ClockIcon color="#b298dc"/>
-      <Text className='text-purple-400 text-2xl'>{length}</Text>
-    </View>
+      <ClockIcon color="#b298dc" size={30}/>
+      <Text className='text-white font-extrabold text-2xl'>{length}</Text>
+    </TouchableOpacity>
   )
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -12,37 +12,46 @@ const HomeScreen = () => {
   return (
 
     <>
-      <ScrollView>
-      <View className='relative mt-10 mx-5'>
-            <View>
-                <WorkoutCard  
-                    title="arm"
-                    length={30}
-                />
-
-                <WorkoutCard  
-                    title="abs"
-                    length={20}
-                />
-
-              <WorkoutCard  
-                    title="abs"
-                    length={20}
-                />
-
-
-              <WorkoutCard  
-                    title="abs"
-                    length={20}
-                />
-
-            </View>
+      <View className='mt-20 ml-5'>
+          <Text className='text-2xl font-extrabold'> Hello!</Text>
         </View>
+      <ScrollView>
+        <View className='relative mx-5 my-5'>
+              <View >
+                  <WorkoutCard  
+                      title="arm"
+                      length={30}
+                      className=''
+                  />
+
+                  <WorkoutCard  
+                      title="arm"
+                      length={30}
+                  />
+
+                  <WorkoutCard  
+                      title="abs"
+                      length={20}
+                  />
+
+                <WorkoutCard  
+                      title="abs"
+                      length={20}
+                  />
+
+
+                <WorkoutCard  
+                      title="abs"
+                      length={20}
+                  />
+
+              </View>
+          </View>
 
       </ScrollView>
 
-      <View className='absolute bottom-10 bg-gray-200'>
-          <TouchableOpacity >
+      <View className='absolute bottom-10 bg-gray-200 w-full'>
+          <TouchableOpacity className='text-center'>
             <PlusCircleIcon  size={50}/>
           </TouchableOpacity>
       </View>
